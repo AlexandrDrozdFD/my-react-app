795d84429f1d25fbc32c93a709d366b759e8bab6
Alexandr Drozd <alex.drozd.fd@gmail.com>
2021-02-19 02:10:20 +0300

    feat: store, state, oop, refactor (...37)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-import state from './redux/state';
-import { renderEntireTree } from './render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-renderEntireTree(state);
+import store from './redux/state';
+
+
+export const renderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        addPostMessage={store.addPostMessage.bind(store)}
+        newPostText={store.updateNewPost.bind(store)}
+        updateNewPost={store.updateNewPost.bind(store)}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+renderEntireTree(store.getState());
+
+store.subscribe(renderEntireTree);
